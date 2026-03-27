573ad7eb7361f49233986250bb34fecf22db25d3
oquctvd <35687143+oquctvd@users.noreply.github.com>
2026-03-27 20:56:12 +0200

    Create hardhat.config.js for Hardhat project

---
+++ smart-contracts/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+};
